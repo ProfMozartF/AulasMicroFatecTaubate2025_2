9a4f9aad6c239863efab141af7fa3533cd45e48b
MozartFM <154269492+MozartFM@users.noreply.github.com>
2025-09-03 19:29:01 -0300

    Mozart: Resolução de exercicos Logica de programação em C

--- Exemplos_CodigoC/Enum.c
+++ Exemplos_CodigoC/Enum.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-enum DiasSemana {DOMINGO, SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA, SABADO};
+enum DiasSemana {DOMINGO =1, SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA, SABADO};
 
 int main() {
     enum DiasSemana hoje = QUARTA;
+    enum DiasSemana ontem = SEGUNDA;
     printf("Hoje = %d\n", hoje);
+    printf("Hoje = %d\n", ontem);
     return 0;
 }
--- Exemplos_CodigoC/Funcao.c
+++ Exemplos_CodigoC/Funcao.c
@@ -1,33 +1,21 @@
 #include <stdio.h>
-//variaveis
-int med_ia;
-int numero_dados;
-int media (int numero_ds); // Protótiopo da função
-
-int main() {
+#include <stdint.h>
+int soma (num1,num2)
+{
+    int soma;
+    soma = num1+num2;
+    return(soma);
     
-    printf("DIGITE NUM ALUNOS: ");
-    scanf("%d", &numero_dados);
-    //med_ia = media(numero_dados);
-    printf("media da sala = %d", media(numero_dados));
-  
-   
-}//FIM DO MAIN
+}
 
-int media(int numero_ds) {
-    int soma=0;
-    int media_local=0;
-    int valor=0;
-    int i=0;
+int main()
+{
+    printf("TESTE SOMA = %d\n",soma(2,3) );
+     printf("TESTE SOMA = %d\n",soma(1,3) );
+      printf("TESTE SOMA = %d\n",soma(2,9) );
+       printf("TESTE SOMA = %d\n",soma(3,5) );
+        printf("TESTE SOMA = %d\n",soma(12,3) );
+         printf("TESTE SOMA = %d\n",soma(23,3) );
    
-      for (i = 0; i < numero_ds; i++)
-      {
-        printf("Digitar a nota: ");
-        scanf("%d", &valor);
-        soma+=valor;
-      }
-    
-    media_local=soma/numero_ds;
-    printf("Na Funcao - Media local = %d\n", media_local);
-    return media_local;
+    return 0;
 }
